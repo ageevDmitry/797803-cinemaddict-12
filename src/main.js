07583a482d18f462d48cf734f3ba6c156f4e6ccb
Dmity <Ageev_Dmity_2016@mail.ru>
2020-08-04 21:10:50 +0300

    удаляет разделы Most Comment и Top Rated

--- src/main.js
+++ src/main.js
@@ -4,8 +4,6 @@ import {createSortFilms} from "./view/sort-films.js";
 import {createFilmsList} from "./view/films-list.js";
 import {createFilmCard} from "./view/films-card.js";
 import {createButtonShowMore} from "./view/button-show-more.js";
-import {createFilmsListTopRated} from "./view/films-list-top-rated.js";
-import {createFilmsListMostCommented} from "./view/films-list-most-comment.js";
 import {createFooterStatistic} from "./view/footer-statistic.js";
 // import {createFilmPopap} from "./view/film-popap.js";
 import {generateFilm} from "./mock/film.js";
@@ -36,24 +34,6 @@ for (let i = 0; i < CARD_FILMS_LIST_COUNT; i++) {
 }
 
 render(filmsList, createButtonShowMore(), `beforeend`);
-
-render(films, createFilmsListTopRated(), `beforeend`);
-
-const filmsListTopRated = siteMain.querySelector(`.films-list--top-rated`);
-const topRatedContainer = filmsListTopRated.querySelector(`.films-list__container`);
-
-for (let i = 0; i < CARD_FILMS_EXTRA_COUNT; i++) {
-  render(topRatedContainer, createFilmCard(), `beforeend`);
-}
-
-render(films, createFilmsListMostCommented(), `beforeend`);
-
-const filmsListMostComment = siteMain.querySelector(`.films-list--most-comment`);
-const mostCommentContainer = filmsListMostComment.querySelector(`.films-list__container`);
-
-for (let i = 0; i < CARD_FILMS_EXTRA_COUNT; i++) {
-  render(mostCommentContainer, createFilmCard(), `beforeend`);
-}
 
 const footer = document.querySelector(`.footer`);
 const footerStatistics = footer.querySelector(`.footer__statistics`);
